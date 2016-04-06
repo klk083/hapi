8d33b6d6cacab27af6caa6b393575cb08bd5de8e
knut erik hildre <knut erik hildre>
2016-04-06 14:01:19 +0200

    .

---
+++ src/Hapi/EditPassword.java
@@ -0,0 +1,24 @@
+package Hapi;
+
+import javax.swing.*;
+
+/**
+ * Created by klk94 on 11.03.2016.
+ */
+public class EditPassword extends JFrame {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton changeButton;
+    private JButton cancelButton;
+    private JPanel editPasswordPanel;
+
+    public EditPassword() {
+        super("EditPassword");
+        setContentPane(editPasswordPanel);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
+
+
---
+++ src/Hapi/tester.java
@@ -0,0 +1,10 @@
+package Hapi;
+
+/**
+ * Created by klk94 on 16.03.2016.
+ */
+public class tester {
+    public static void main(String[] args) {
+        LoggIn tester = new LoggIn();
+    }
+}
