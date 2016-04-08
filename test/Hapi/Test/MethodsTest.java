274bb8f056695cf87cf2192fba5cf707958345a5
magnussh <magnussh@student.hist.no>
2016-04-08 10:05:37 +0200

    Added JUnit tests.

---
+++ test/Hapi/Test/MethodsTest.java
@@ -0,0 +1,86 @@
+package Hapi.Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 08.04.2016.
+ */
+public class MethodsTest {
+
+    @Test
+    public void testLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testChangePassword() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateUser() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+
+    }
+
+    @Test
+    public void testListCustomers() throws Exception {
+
+    }
+
+    @Test
+    public void testListCustomers1() throws Exception {
+
+    }
+
+    @Test
+    public void testListEmployees() throws Exception {
+
+    }
+
+    @Test
+    public void testListEmployees1() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateCustomer() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCustomerDiscount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRoleID() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testAddMenuToOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testAddSubToOrder() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveMenuFromOrder() throws Exception {
+
+    }
+}
