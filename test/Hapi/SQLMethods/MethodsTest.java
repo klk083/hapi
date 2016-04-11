047d8b106cf0b5eff1a7ba08ef8c4454e7137846
magnussh <magnussh@student.hist.no>
2016-04-11 12:36:34 +0200

    New JUnit-test.

---
+++ test/Hapi/SQLMethods/MethodsTest.java
@@ -0,0 +1,101 @@
+package Hapi.SQLMethods;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 11.04.2016.
+ */
+public class MethodsTest {
+    @Test
+    public void login() throws Exception {
+
+    }
+
+    @Test
+    public void changePassword() throws Exception {
+
+    }
+
+    @Test
+    public void createUser() throws Exception {
+
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+
+    }
+
+    @Test
+    public void listCustomers() throws Exception {
+
+    }
+
+    @Test
+    public void listCustomers1() throws Exception {
+
+    }
+
+    @Test
+    public void listEmployees() throws Exception {
+
+    }
+
+    @Test
+    public void listEmployees1() throws Exception {
+
+    }
+
+    @Test
+    public void createCustomer() throws Exception {
+
+    }
+
+    @Test
+    public void setCustomerDiscount() throws Exception {
+
+    }
+
+    @Test
+    public void getRoleID() throws Exception {
+
+    }
+
+    @Test
+    public void createOrder() throws Exception {
+
+    }
+
+    @Test
+    public void addMenuToOrder() throws Exception {
+
+    }
+
+    @Test
+    public void addSubToOrder() throws Exception {
+
+    }
+
+    @Test
+    public void removeMenuFromOrder() throws Exception {
+
+    }
+
+    @Test
+    public void addIngredient() throws Exception {
+
+    }
+
+    @Test
+    public void removeIngredient() throws Exception {
+
+    }
+
+    @Test
+    public void createMenu() throws Exception {
+
+    }
+
+}
