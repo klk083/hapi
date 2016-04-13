77ed86387c84b82b3c9c7e6ea46ae25b42bff9c3
keennetk <keennetk@student.hist.no>
2016-04-13 14:22:52 +0200

    test

--- src/Hapi/GUI/Customer/EditCustomer.java
+++ src/Hapi/GUI/Customer/EditCustomer.java
@@ -10,18 +10,22 @@ import java.awt.event.ActionListener;
 public class EditCustomer extends JFrame {
     private JButton cancleButton;
     private JButton changeButton;
-    private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JTextField textField4;
     private JPanel EditCustomerPannel;
+    private JLabel oldPhone;
+    private JLabel oldAdress;
 
-    public EditCustomer(String selectedValue) {
+    public EditCustomer(String adress, String tlfNr) {
         super("Create customer");
         setContentPane(EditCustomerPannel);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        oldAdress.setText(adress);
+        oldPhone.setText(tlfNr);
+
         cancleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
