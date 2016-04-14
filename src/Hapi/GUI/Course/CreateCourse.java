1abf1cf70bec2e6b8f4761132944a157379844c7
keennetk <keennetk@student.hist.no>
2016-04-14 10:17:59 +0200

    Courses

--- src/Hapi/GUI/Course/CreateCourse.java
+++ src/Hapi/GUI/Course/CreateCourse.java
@@ -7,13 +7,17 @@ import javax.swing.*;
  */
 public class CreateCourse {
     private JTextField textField1;
-    private JButton button1;
+    private JButton searchButton;
     private JList list1;
     private JList list2;
-    private JButton createIngredientButton;
+    private JButton createIngredientChangeIngredientButton;
     private JButton addButton;
     private JButton removeButton;
     private JTextField textField3;
     private JTextField textField2;
-    private JButton button2;
+    private JButton mainMenuButton;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JTextField textField5;
+    private JButton createButton;
 }
