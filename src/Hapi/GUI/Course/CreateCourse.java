2edc47c9a322335238d5b3dfc6f82389f42ff167
Håkon <haakbe@student.hist.no>
2016-04-14 14:26:21 +0200

    design customer/user

--- src/Hapi/GUI/Course/CreateCourse.java
+++ src/Hapi/GUI/Course/CreateCourse.java
@@ -9,15 +9,15 @@ public class CreateCourse {
     private JTextField textField1;
     private JButton searchButton;
     private JList list1;
-    private JList list2;
     private JButton createIngredientChangeIngredientButton;
     private JButton addButton;
     private JButton removeButton;
     private JTextField textField3;
     private JTextField textField2;
-    private JButton mainMenuButton;
+    private JButton backButton;
     private JTextField textField4;
     private JComboBox comboBox1;
     private JTextField textField5;
     private JButton createButton;
+    private JList list2;
 }
