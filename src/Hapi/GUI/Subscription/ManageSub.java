5100b5fba897d452320b2e537448eb235c49f065
Knut <knute_knut@hotmail.com>
2016-04-14 14:18:41 +0200

    .

--- src/Hapi/GUI/Subscription/ManageSub.java
+++ src/Hapi/GUI/Subscription/ManageSub.java
@@ -1,11 +1,12 @@
 package Hapi.GUI.Subscription;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by klk94 on 13.03.2016.
  */
-public class ManageSub {
+public class ManageSub extends JFrame {
     private JButton deleteSubButton;
     private JButton editSubButton;
     private JButton backButton;
@@ -14,4 +15,16 @@ public class ManageSub {
     private JTextField textField1;
     private JButton searchButton;
     private JButton viewSubButton;
+    private JPanel ManageSubPannel;
+
+    public ManageSub() {
+        super("Create customer");
+        setContentPane(ManageSubPannel);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+
+    }
 }
