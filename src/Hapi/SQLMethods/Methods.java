9c30fb3c9e5b386c15a8e9421a7d5fd8adc82aab
magnussh <magnussh@student.hist.no>
2016-04-14 12:43:50 +0200

    Error fix.

--- src/Hapi/SQLMethods/Methods.java
+++ src/Hapi/SQLMethods/Methods.java
@@ -46,7 +46,7 @@ public class Methods {
         int output = -1;
         try {
             con = SQLConnection.openConnection();
-            String selectSQL = "SELECT sum(price) FROM ingredient NATURAL JOIN menu_ingredient WHERE menu_id = ?";
+            String selectSQL = "SELECT sum(price) AS price FROM ingredient NATURAL JOIN menu_ingredient WHERE menu_id = ?";
             stm = con.prepareStatement(selectSQL);
             stm.setInt(1, menuID);
             res = stm.executeQuery();
