8ffdec9a4e0348f7077b2e005dceb6882433af3f
Håkon <haakbe@student.hist.no>
2016-04-14 14:15:53 +0200

    design customer/user

--- src/Hapi/GUI/Subscription/ManageSub.java
+++ src/Hapi/GUI/Subscription/ManageSub.java
@@ -6,9 +6,12 @@ import javax.swing.*;
  * Created by klk94 on 13.03.2016.
  */
 public class ManageSub {
-    private JList list1;
-    private JButton deleteSubsrciptionButton;
-    private JButton editSubscriptionButton;
+    private JButton deleteSubButton;
+    private JButton editSubButton;
     private JButton backButton;
-    private JButton addSubscriptionButton;
+    private JButton createSubscriptionButton;
+    private JList list1;
+    private JTextField textField1;
+    private JButton searchButton;
+    private JButton viewSubButton;
 }
