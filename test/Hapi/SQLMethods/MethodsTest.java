8fa03afb96479d9c407a908bc0cbf31008d7c3a4
magnussh <magnussh@student.hist.no>
2016-04-15 10:02:16 +0200

    Isolated JUnit-tests, and fixed them.

--- test/Hapi/SQLMethods/MethodsTest.java
+++ test/Hapi/SQLMethods/MethodsTest.java
@@ -1,6 +1,8 @@
 package Hapi.SQLMethods;
 
+import org.junit.After;
 import org.junit.Test;
+import org.junit.Before;
 
 import java.util.ArrayList;
 
@@ -12,18 +14,26 @@ import static org.junit.Assert.*;
 public class MethodsTest {
     // All tests are supposed to be run on a database built with our database.sql and test data given in the same file
 
+    @Before
+    public void before() throws Exception {
+        String username = "testuser", password = "test", name = "Tester McTest";
+        int role = 1;
+        Methods.createUser(username, password, name, role);
+    }
+
     @Test
     public void createUser() throws Exception {
-        String username = "test-test", password = "test", name = "Tester McTest";
+        String username = "tester", password = "test", name = "Tester McTest";
         int role = 1;
         boolean test = Methods.createUser(username, password, name, role);
+        Methods.deleteUser(username);
 
         assertEquals(true, test);
     }
 
     @Test
     public void login1() throws Exception {
-        boolean test = Methods.login("test-test", "test");
+        boolean test = Methods.login("testuser", "test");
 
         assertEquals(true, test);
     }
@@ -38,69 +48,86 @@ public class MethodsTest {
 
     @Test
     public void changePassword() throws Exception {
-        String username ="test-test", password = "test-test";
-        boolean test = Methods.changePassword(username, password);
+        String username = "tester", password = "test", name = "Tester McTest";
+        int role = 1;
+        Methods.createUser(username, password, name, role);
 
+        String password2 = "test-test";
+        boolean test = Methods.changePassword(username, password2);
+
+        Methods.deleteUser(username);
         assertEquals(true, test);
     }
 
     @Test
     public void login3() throws Exception {
-        // This login tries to login with old password for username "test-test"
-        boolean test = Methods.login("test-test", "test");
+        // This login tries to login with wrong password for username "testuser"
+        boolean test = Methods.login("testuser", "12345678");
 
         assertEquals(false, test);
     }
 
     @Test
     public void login4() throws Exception {
-        boolean test = Methods.login("test-test", "test-test");
+        String username = "tester", password = "test", name = "Tester McTest";
+        int role = 1;
+        Methods.createUser(username, password, name, role);
 
-        assertEquals(true, test);
+        String password2 = "test-test";
+        Methods.changePassword(username, password2);
+
+        boolean testRes = Methods.login(username, password2);
+
+        Methods.deleteUser(username);
+        assertEquals(true, testRes);
     }
 
     @Test
     public void listEmployees1() throws Exception {
-        // This test checks if user is found in the database
-        String username = "test-test";
+        // This test checks if username is found in the database
+        String username = "testuser";       // User from @Before
 
         ArrayList<ArrayList<String>> list = Methods.listEmployees(username);
 
-        assertEquals("test-test", list.get(0).get(0));
+        assertEquals("testuser", list.get(0).get(0));
     }
 
     @Test
     public void listEmployees2() throws Exception {
         // This test checks if a users employee_id is found in the database
-        String username = "test-test";
+        String username = "testuser";       // User from @Before
 
         ArrayList<ArrayList<String>> list = Methods.listEmployees(username);
-        int temp = Integer.parseInt(list.get(1).get(0));
-        assertEquals(2, temp);      // User made in tests gets ID no. 2, this is because admin-user always gets ID no. 1
+        int testRes = Integer.parseInt(list.get(1).get(0));
+
+        assertEquals(2, testRes);   // User made in tests gets ID no. 2, this is because admin-user always gets ID no. 1
     }
 
     @Test
     public void getRoleID() throws Exception {
-        String username = "test-test";
+        // This test checks if a users role_id is found in the database
+        String username = "testuser";       // User from @Before
 
-        int temp = Methods.getRoleID(username);
+        int testRes = Methods.getRoleID(username);
 
-        assertEquals(1, temp);
+        assertEquals(1, testRes);
     }
 
     @Test
     public void deleteUser() throws Exception {
-        String username = "test-test";
-        boolean test = Methods.deleteUser(username);
+        String username = "tester", password = "test", name = "Tester McTest";
+        int role = 1;
+        Methods.createUser(username, password, name, role);
 
-        assertEquals(true, test);
+        boolean testRes = Methods.deleteUser(username);
+
+        assertEquals(true, testRes);
     }
 
     @Test
     public void listCustomers() throws Exception {
 
     }
-
 
     @Test
     public void listEmployees() throws Exception {
@@ -152,4 +179,10 @@ public class MethodsTest {
 
     }
 
+
+
+    @After
+    public void after() throws Exception {
+
+    }
 }
