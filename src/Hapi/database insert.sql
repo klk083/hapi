2d976b3b58a386e2b3242d9abc4436fd4c61bbc3
magnussh <magnussh@student.hist.no>
2016-04-20 12:29:38 +0200

    Corrected hash and salt of new users.

--- src/Hapi/database insert.sql
+++ src/Hapi/database insert.sql
@@ -1,9 +1,7 @@
 # noinspection SqlNoDataSourceInspectionForFile
 INSERT INTO employee VALUES(DEFAULT, 1, "admin", "admin", "$6$jv450qggfl621$nFLSFodaU3qTvW51TGSIebgwqPKEnl5UeYN9J2FQfcRPmSxgAcPXPdUS9f7WaZGGgnbQPPwtwTf2mrSYbYGhh0", "$6$jv450qggfl621");
-INSERT INTO employee VALUES(DEFAULT, 4, "Ivar", "ivar", "$6$jv450qggfl621$nFLSFodaU3qTvW51TGSIebgwqPKEnl5UeYN9J2FQfcRPmSxgAcPXPdUS9f7WaZGGgnbQPPwtwTf2mrSYbYGhh0", "$6$inrd2vt1sadn9");
-INSERT INTO employee VALUES(DEFAULT, 4, "Luft lars", "luft", "$6$inrd2vt1sadn9$KGRzE/G2JZf6dXvOFvvTYDiro/avbSPPL.aZWaIjAFFaHCZR7ZZZU.0hT6TJjUztp0U0ARuGiiyDeb5Vw75A.1", "$6$6vuulpp2sbk96");
-
-
+INSERT INTO employee VALUES(DEFAULT, 4, "Ivar", "ivar", "$6$inrd2vt1sadn9$KGRzE/G2JZf6dXvOFvvTYDiro/avbSPPL.aZWaIjAFFaHCZR7ZZZU.0hT6TJjUztp0U0ARuGiiyDeb5Vw75A.1", "$6$inrd2vt1sadn9");
+INSERT INTO employee VALUES(DEFAULT, 4, "Luft lars", "luft", "$6$21ne21aunh713$rImoLoSGxio7xV.BbLP.O7Vx3Ps6TaeTIc0Qcr6OfhMtLIN02X5Qo7xL6U2RRxdTgEc//dVDGcznCmS4390Zn0", "$6$21ne21aunh713");
 
 
 INSERT INTO role VALUES(DEFAULT, 'CEO');
